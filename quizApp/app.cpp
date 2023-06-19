416b38937a32a5a06902a25719f735a0a6ec688d
Fady2007 <96028450+Fady2007@users.noreply.github.com>
2023-06-19 15:53:29 +0300

    Add files via upload

---
+++ quizApp/app.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int second() 
+{
+    cout << "Come From second function";
+    return 0;
+}
+
+int main()
+{
+    cout << "\nHi\nI hate c++ so much\nBye\n";
+    second();
+    return 0;
+}
